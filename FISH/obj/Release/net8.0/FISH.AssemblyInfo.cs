120e900b93b3627db1f6adbb8b8e0d739692b038
JasonWang <JasonWang@g-mate.org>
2024-07-28 18:15:27 +0800

    ICON 建立
    增加手機  增加網站加入桌面 變成APP 形式

--- FISH/obj/Release/net8.0/FISH.AssemblyInfo.cs
+++ FISH/obj/Release/net8.0/FISH.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FISH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+915319c1e77e1febf98e6642f6f5196d3461255c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1408da8e74601928f91024d6074919ce4ad5bea5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FISH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FISH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ FISH/wwwroot/js/service-worker.js
@@ -0,0 +1,13 @@
+﻿self.addEventListener('install', event => {
+    console.log('Service Worker installing.');
+    // 可以在這裡預緩存資源
+});
+
+self.addEventListener('activate', event => {
+    console.log('Service Worker activating.');
+});
+
+self.addEventListener('fetch', event => {
+    console.log('Fetching:', event.request.url);
+    // 可以在這裡處理資源請求
+});
